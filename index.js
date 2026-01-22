01f0dcc188aabff2a07eecd26cb6198a0e2bdd65
Sahil-late <latesahil67@gmail.com>
2026-01-22 13:06:35 +0530

    production change

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, 'public')))
